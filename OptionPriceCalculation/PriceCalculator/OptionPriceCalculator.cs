a24a2e2ec8c7548ed3fd06b7b0dfa973b63152c1
arboktya <howaboutno129@gmail.com>
2023-12-11 15:10:58 +0300

    рефакторинг

--- OptionPriceCalculation/PriceCalculator/OptionPriceCalculator.cs
+++ OptionPriceCalculation/PriceCalculator/OptionPriceCalculator.cs
@@ -3,18 +3,21 @@
     internal class OptionPriceCalculator
     {
         private OptionPriceCalculationMethod calcMethod { get; set; }
-        public OptionParams OptionParams { get; set; }
+        private OptionParams optionParams { get; set; }
 
         public OptionPriceCalculator(OptionPriceCalculationMethod calcMethod,
             OptionParams optionParams)
         {
             this.calcMethod = calcMethod;
-            OptionParams = optionParams;
+            this.optionParams = optionParams;
         }
 
-        public double Calculate() => calcMethod.Calculate(OptionParams);
+        public double Calculate() => calcMethod.Calculate(optionParams);
 
         public void SetMethod(OptionPriceCalculationMethod calcMethod)
             => this.calcMethod = calcMethod;
+
+        public void SetOptionParams(OptionParams optionParams)
+            => this.optionParams = optionParams;
     }
 }
